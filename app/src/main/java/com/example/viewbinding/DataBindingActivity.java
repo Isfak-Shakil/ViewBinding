cc14bddf1f4b65937a847b91b58badf28261a43d
USER <isfakshakil@gmail.com>
2022-10-03 20:29:20 +0600

    liveData

--- app/src/main/java/com/example/viewbinding/DataBindingActivity.java
+++ app/src/main/java/com/example/viewbinding/DataBindingActivity.java
@@ -2,6 +2,7 @@ package com.example.viewbinding;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
@@ -22,20 +23,26 @@ public class DataBindingActivity extends AppCompatActivity {
         bindingB= DataBindingUtil.setContentView(this,R.layout.activity_data_binding);
 
         MyViewModel myViewModel=new ViewModelProvider(this).get(MyViewModel.class);
-  bindingB.textViewId.setText(String.valueOf(myViewModel.score));
+ // bindingB.textViewId.setText(String.valueOf(myViewModel.score));
+        myViewModel.score.observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                bindingB.textViewId.setText(String.valueOf(myViewModel.score.getValue()));
+            }
+        });
 
         bindingB.addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                  myViewModel.increment();
-                bindingB.textViewId.setText(String.valueOf(myViewModel.score));
+               // bindingB.textViewId.setText(String.valueOf(myViewModel.score));
             }
         });
         bindingB.minBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 myViewModel.decrement();
-                bindingB.textViewId.setText(String.valueOf(myViewModel.score));
+              //  bindingB.textViewId.setText(String.valueOf(myViewModel.score));
             }
         });
 
---
+++ app/src/main/java/com/example/viewbinding/MyViewModel.java
@@ -0,0 +1,28 @@
+package com.example.viewbinding;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MyViewModel extends ViewModel {
+  MutableLiveData<Integer> score=new MutableLiveData<>();
+    public MyViewModel() {
+        score.setValue(0);
+    }
+    public void increment(){
+        score.setValue(score.getValue()+1);
+    }
+    public void decrement(){
+        score.setValue(score.getValue()-1);
+    }
+
+
+//      int score=0;
+//   public MyViewModel() {
+//   }
+//    public void increment(){
+//        score++;
+//    }
+//    public void decrement(){
+//        score--;
+//    }
+}
